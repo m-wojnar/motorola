1d476abb6e87ed27c0126eea843b442670138061
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-07-12 15:33:59 +0200

    Nauka wypisywania na ekran i tworzenia komentarzy

---
+++ czesc_2.js
@@ -0,0 +1,12 @@
+// jak wypisać wiadomość do konsoli
+// zwróć uwagę na średnik na końcu instrukcji
+console.log('Hello world!');
+
+// jak napisać komentarz
+// to jest komentarz jednolinijkowy
+
+/*
+to jest komentarz wielolinijkowy
+to jest komentarz wielolinijkowy
+to jest komentarz wielolinijkowy
+*/
